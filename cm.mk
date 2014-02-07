d60da3c6ecf33915d137a036ff97ddd47d4df33c
Michael Bestas <mikeioannina@gmail.com>
2014-02-07 17:08:31 +0200

    haida: Fix device name
    Change-Id: I624aa34882893b291669606e7061cc4bd319c15e

--- cm.mk
+++ cm.mk
@@ -22,8 +22,14 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=MT11i BUILD_FINGERPRINT="SEMC/MT11i_1254-2184/MT11i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" PRIVATE_BUILD_DESC="MT11i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=MT11i \
+    PRODUCT_DEVICE=MT11i \
+    BUILD_FINGERPRINT="SEMC/MT11i_1254-2184/MT11i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" \
+    PRIVATE_BUILD_DESC="MT11i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_haida
-PRODUCT_DEVICE := haida
+
+# Release name
+PRODUCT_RELEASE_NAME := haida
